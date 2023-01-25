005f5b77a6dc1da212c20f6a8e31a802b699a117
JesusRueda1 <jdrueda83@misena.edu.co>
2023-01-25 15:13:48 -0500

    Menu hamburguesa arreglado

--- Vigilantes/Home/inicio.php
+++ Vigilantes/Home/inicio.php
@@ -134,11 +134,11 @@ include('../../Configuracion/Validar/validar_sesion.php');
 
         <script src="../../Vista/assets/js/bootstrap.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="Vista/assets/demo/chart-area-demo.js"></script>
-        <script src="Vista/assets/demo/chart-bar-demo.js"></script>
-        <script src="Vista/assets/demo/chart-pie-demo.js"></script>
+        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script> -->
+        <!-- <script src="../Vista/assets/demo/chart-area-demo.js"></script>
+        <script src="../Vista/assets/demo/chart-bar-demo.js"></script>
+        <script src="../Vista/assets/demo/chart-pie-demo.js"></script> -->
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-        <script src="Vista/assets/js/scripts.js"></script>
+        <script src="../Vista/js/scripts.js"></script>
     </body>
 </html>
